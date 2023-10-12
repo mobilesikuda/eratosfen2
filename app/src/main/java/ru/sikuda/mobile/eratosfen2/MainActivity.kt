4e56dcdcc6ab44d56f82f77f153a7ce0cca35fef
ya.sikuda <ya.sikuda@yandex.ru>
2023-10-12 17:49:59 +0300

    Initial commit

---
+++ app/src/main/java/ru/sikuda/mobile/eratosfen2/MainActivity.kt
@@ -0,0 +1,112 @@
+package ru.sikuda.mobile.eratosfen2
+
+import android.app.AlertDialog
+import android.content.ContentValues
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.getString
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ru.sikuda.mobile.eratosfen2.ui.theme.Eratosfen2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Eratosfen2Theme {
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainView()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainView(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var textClickButton by remember { mutableStateOf(getString(context, R.string.click_me)) }
+    val viewModel = viewModel<MainViewModel>()
+    val timeText by viewModel.text.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+
+        Button(onClick = {
+            textClickButton = "Clickme-" + Math.random().toString()
+        }) {
+            Text(text = textClickButton)
+        }
+        Box(modifier.fillMaxHeight(0.2F))
+        Text(
+            text = timeText,
+            modifier = modifier
+        )
+        Button(onClick = {
+            val listener = DialogInterface.OnClickListener { _, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> viewModel.RunCalcSync()
+                    DialogInterface.BUTTON_NEGATIVE,
+                    DialogInterface.BUTTON_NEUTRAL -> {
+                        Toast.makeText(context, "May be next time", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            val dialog = AlertDialog.Builder(context)
+                .setCancelable(true)
+                .setIcon(R.mipmap.ic_launcher_round)
+                .setTitle(R.string.result_title)
+                .setMessage(R.string.result_message)
+                .setPositiveButton(R.string.action_yes, listener)
+                .setNeutralButton(R.string.action_ignore, listener)
+                .setOnDismissListener {
+                    Log.d(ContentValues.TAG, "Dialog dismissed")
+                }
+                .create()
+
+            dialog.show()
+        }) {
+            Text(text = getString(context, R.string.count))
+        }
+        Button(
+            onClick = { viewModel.RunCalc() }) {
+            Text(text = getString(context,R.string.count_async))
+        }
+
+    }
+
+}
+
---
+++ app/src/main/java/ru/sikuda/mobile/eratosfen2/MainViewModel.kt
@@ -0,0 +1,60 @@
+package ru.sikuda.mobile.eratosfen2
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel: ViewModel() {
+
+    private val _text = MutableStateFlow("")
+    val text = _text.asStateFlow()
+
+    //run async routing
+    fun RunCalc(){
+        viewModelScope.launch {
+            _text.value = "Async calc..."
+            val result = withContext( Dispatchers.Default ) {
+                return@withContext Calculate()
+            }
+            _text.value = "Async-"+result
+        }
+    }
+
+    //run sync routing
+    fun RunCalcSync(){
+        _text.value = "Sync calc..."
+        _text.value = "Sync-"+Calculate()
+    }
+
+    //run execution
+    private fun Calculate(): String {
+
+        val n = 50_000_000
+        val array: Array<Int> = Array(n+1) { 1 }
+        array[0] = 0
+        array[1] = 0
+
+        val timeBegin = System.currentTimeMillis()
+        var i = 2
+        while ( i <= n ) {
+            if (array[i] == 1) {
+                val sq: Long = i.toLong() * i
+                if (sq <= n) {
+                    var m: Int = sq.toInt()
+                    while (m <= n) {
+                        array[m] = 0
+                        m += i
+                    }
+                }
+            }
+            i += 1
+        }
+        val timeEnd = System.currentTimeMillis()
+        val diff: Double = (timeEnd.toDouble() - timeBegin) / 1000
+        return diff.toString()
+    }
+}
