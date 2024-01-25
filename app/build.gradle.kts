aa0fe7392aef81553e648b37bd6bd937bbb26d33
sikudamobile <ya.sikuda@yandex.ru>
2024-01-25 09:34:16 +0300

    minSdk = 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.sikuda.mobile.eratosfen2"
-        minSdk = 27
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
